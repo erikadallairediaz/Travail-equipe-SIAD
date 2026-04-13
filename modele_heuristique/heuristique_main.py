defedb1ec35668a30bdb17b947ca768b34bafcba
SophieIraca <soira3@ulaval.ca>
2026-04-12 20:02:25 -0400

    main

--- modele_heuristique/heuristique_main.py
+++ modele_heuristique/heuristique_main.py
@@ -1,19 +1,28 @@
 from .probleme_heur import ProblemeHeur 
 from .minizinc_solver import MiniZincSolver
+import time
 
 
 def main():
     solver = MiniZincSolver("coin-bc")
 
-    # boucler sur les 3 instances
     for inst in [1, 2, 3]:
-        print(f"\nINSTANCE {inst}\n")
+        print("\n" + "="*50)
+        print(f"INSTANCE {inst}")
+        print("="*50)
 
         prob = ProblemeHeur(creation_instance=inst)
+
+        start = time.time()  # début du chrono
+
         sol = solver.solve(prob)
+
+        end = time.time()  # fin du chrono
 
         print("Valeur fct objectif :", sol.evaluate())
         print("Solution valide ?", sol.validate())
+        print(f"Temps de résolution : {end - start:.4f} secondes")
+
         sol.print_propre()
 
 
